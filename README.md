3b8d64ab15ca7e5a759c9f3b155fa1b527e6c357
Francisco Manríquez Novoa <49853152+CapitanPunch@users.noreply.github.com>
2021-10-08 15:45:56 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# facturacion_dimerc
+PLANIFICACIÓN
+
+- SEMANA 1 (Lunes 27/09 – Viernes 01/10):
+	- Inicio del proyecto
+	- Reconocer RUTs en los textos
+
+- SEMANA 2 (Lunes 04/10 – Viernes 08/10):
+	- Reconocer números de factura en los textos
+	- Categorizar transacciones
+	- Mejorar reconocimientos anteriores
+
+- SEMANA 3 (Lunes 11/10 – Viernes 15/10):
+	- Plantear propuesta actual de manera presencial
+	- Corregir detalles en caso de existir
+	-
+
+- SEMANA 4 (Lunes 18/10 – Viernes 22/10):
+	- Empezar segunda parte del proyecto
+	- Obtener información sobre facturas
+	- Realizar calce de informaciones
+
+- SEMANA 5 (Lunes 25/10 – Viernes 29/10):
+	-
+	-
+	-
+
+- SEMANA 6 (Lunes 01/11 – Viernes 05/11):
+	-
+	-
+	-
+
+- SEMANA 7 (Lunes 08/11 – Viernes 12/11):
+	-
+	-
+	-
+
+- SEMANA 8 (Lunes 15/11 – Viernes 19/11):
+	-
+	-
+	-
---
+++ main.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import numpy as np
+import re
+import os
+from funciones import *
+
+fecha_inicio  = input("Fecha de inicio (DD.MM.AAAA): ")
+fecha_termino = "30.09.2021"
+directorio = "".join((fecha_inicio, "-", fecha_termino, "/"))
+
+try:
+	os.mkdir(directorio)
+except FileExistsError:
+	pass
+
+dataframe = leer_archivo(directorio + "ORIGINAL.xlsx")
+
+textos = dataframe["Texto"]
+dataframe["Categoría"] = textos.apply(detectar_categoria)
+print("Categoría detectada.")
+dataframe["RUT"] = textos.apply(detectar_rut)
+print("RUT detectado.")
+dataframe["N° Factura"] = textos.apply(detectar_n_factura)
+print("N° de factura detectado.")
+dataframe["Números"] = textos.apply(detectar_numeros)
+
+"""
+dataframe["Categoría"] = dataframe["Texto"].apply(detectar_categoria)
+dataframe["RUT"] = dataframe["Texto"].apply(detectar_rut)
+
+filtro = pd.isna(dataframe["RUT"])
+dataframe["N° Factura"] = np.nan
+dataframe.loc[filtro, "N° Factura"] = dataframe[filtro]["Texto"].apply(detectar_n_factura)
+"""
+
+con_rut = dataframe[pd.notna(dataframe["RUT"])]
+con_n_factura = dataframe[pd.isna(dataframe["RUT"]) & pd.notna(dataframe["N° Factura"])]
+con_otros_numeros = dataframe[pd.isna(dataframe["RUT"]) & pd.isna(dataframe["N° Factura"]) & pd.notna(dataframe["Números"])]
+sin_numero = dataframe[pd.isna(dataframe["RUT"]) & pd.isna(dataframe["N° Factura"]) & pd.isna(dataframe["Números"])]
+
+"""
+print("TODOS LOS DATOS\n")
+print(dataframe)
+print("\n\nCON RUT\n")
+print(con_rut)
+print("\n\nCON N° DE FACTURA\n")
+print(con_n_factura)
+print("\n\nCON OTROS NÚMEROS\n")
+print(con_otros_numeros)
+print("\n\nSIN NÚMERO\n")
+print(sin_numero)
+"""
+
+for df, nombre in ((dataframe, "NUEVO"), (con_rut, "RUT"), (con_n_factura, "N_FACTURA"), (con_otros_numeros, "OTROS_N"), (sin_numero, "SIN_N")):
+	escribir_archivo(df, directorio + nombre + ".xlsx")
