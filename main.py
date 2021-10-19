e9620debc1fadc1a122b639780bdd7ce47351ff1
Francisco Manríquez Novoa <49853152+CapitanPunch@users.noreply.github.com>
2021-10-19 11:35:37 -0300

    Se inició la parte 2 y se agregó un Notebook

--- main.py
+++ main.py
@@ -30,3 +30,24 @@ for dataframe, nombre in ((FBL3N, "NUEVO"), (con_rut, "RUT"), (con_n_factura, "N
 """
 
 
+# PARTE 2
+
+FBL5N = leer_excel("FBL5N.xlsx")
+FBL5N["Cuenta"] = FBL5N["Cuenta"].apply(lambda n: str(n).strip("0"))
+
+# Calzar números de factura
+
+matches_factura = pd.merge(FBL5N, con_n_factura, left_on="Nｺ doc.", right_on="Nº documento")
+print(matches_factura)
+escribir_excel(matches_factura, "MATCHES.xlsx")
+
+
+# Calzar RUTs
+KNA1 = leer_excel("KNA1.xlsx")
+KNA1["Nº ident.fis.1"] = KNA1["Nº ident.fis.1"].apply(lambda s: str(s).replace("-", ""))
+
+join = pd.merge(FBL5N, KNA1, left_on="Cuenta", right_on="Cliente")
+join = FBL5N.merge(KNA1, left_on="Cuenta", right_on="Cliente", how='left')
+#join = pd.merge(join, con_rut, left_on="Nº ident.fis.1", right_on="RUT")
+escribir_excel(join, "Join.xlsx")
+
